8dd56768a9889a10bfd1c8df7daeb87eb78ea285
Carlos Martinez <curlosmart@gmail.com>
2022-02-07 22:11:33 -0500

    added navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className="border-b border-gray-300 flex justify-between px-10 py-4">
+      <div>
+        <h1>Carlos Martinez</h1>
+      </div>
+
+      <div className="space-x-5">
+        <span>Home</span>
+        <span>About Me</span>
+        <span>Skills</span>
+        <span>Projects</span>
+        <span>Education & Experience</span>
+      </div>
+    </div>
+  )
+};
+
+export default Navbar;
